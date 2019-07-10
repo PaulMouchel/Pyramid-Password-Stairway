fffcd6a031a1a5ed7fbd11b7a86799b4641bdee9
PaulMouchel <paulmouchel@live.fr>
2019-07-10 16:55:12 +0200

    pyramids last release

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -2,48 +2,34 @@
 def ask_pyramid_size
 	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 	print "> "
-	return gets.to_i
+	h = gets.to_i
+	while (h%2 == 0 || h<1 || h>25)
+		puts "Saisis un nombre impair entre 1 et 25 stp !"
+		print "> "
+		h = gets.to_i
+	end
+	return h
 end
 
 def half_pyramid
 	hauteur = ask_pyramid_size
-	if (hauteur >= 1) && (hauteur <= 25)
-		puts "Voici la pyramide :"
-		1.upto(hauteur) do |i|
-			puts " "*(hauteur - i) + "#"*i
-		end
-	end
+	puts "Voici la pyramide :"
+	1.upto(hauteur) { |i| puts " "*(hauteur - i) + "#"*i }
 end
-
 
 def full_pyramid
 	hauteur = ask_pyramid_size
-	if (hauteur >= 1) && (hauteur <= 25)
-		puts "Voici la pyramide :"
-		1.upto(hauteur) do |i|
-			puts " "*(hauteur - i) + "#"*((i-1)*2 + 1)
-		end
-	end
+	puts "Voici la pyramide :"
+	1.upto(hauteur) { |i| puts " "*(hauteur - i) + "#"*((i-1)*2 + 1) }
 end
 
 def wtf_pyramid
 	hauteur = ask_pyramid_size
-	if hauteur%2 == 0
-		puts "Saisis un nombre impair stp !"
-	else
-		if (hauteur >= 1) && (hauteur <= 25)
-			puts "Voici la pyramide :"
-			1.upto(hauteur) do |i|
-				puts " "*(hauteur - i) + "#"*((i-1)*2 + 1)
-			end
-			(hauteur-1).downto(1) do |i|
-				puts " "*(hauteur - i) + "#"*((i-1)*2 + 1)
-			end
-		end
-	end
+	puts "Voici la pyramide :"
+	1.upto(hauteur) { |i| puts " "*(hauteur - i) + "#"*((i-1)*2 + 1) }
+	(hauteur-1).downto(1) { |i| puts " "*(hauteur - i) + "#"*((i-1)*2 + 1) }
 end
 
-
-#half_pyramid
-#full_pyramid
+half_pyramid
+full_pyramid
 wtf_pyramid
