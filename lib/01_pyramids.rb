f735bceb3c27cdbf8ce29fbe3860a61be59d522a
PaulMouchel <paulmouchel@live.fr>
2019-07-10 14:49:59 +0200

    modifications on pyramids

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,85 +1,49 @@
 
-def half_pyramid
+def ask_pyramid_size
 	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 	print "> "
-	hauteur = gets.to_i
+	return gets.to_i
+end
 
+def half_pyramid
+	hauteur = ask_pyramid_size
 	if (hauteur >= 1) && (hauteur <= 25)
 		puts "Voici la pyramide :"
 		1.upto(hauteur) do |i|
-			a = hauteur - i
-			a.times do
-				print " "
-			end
-			i.times do
-				print "#"
-			end
-			puts ""
+			puts " "*(hauteur - i) + "#"*i
 		end
 	end
 end
 
 
 def full_pyramid
-	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
-	print "> "
-	hauteur = gets.to_i
-
+	hauteur = ask_pyramid_size
 	if (hauteur >= 1) && (hauteur <= 25)
 		puts "Voici la pyramide :"
 		1.upto(hauteur) do |i|
-			a = hauteur - i
-			a.times do
-				print " "
-			end
-			((i-1)*2 + 1).times do
-				print "#"
-			end
-			a.times do
-				print " "
-			end
-			puts ""
+			puts " "*(hauteur - i) + "#"*((i-1)*2 + 1)
 		end
 	end
 end
 
 def wtf_pyramid
-	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
-	print "> "
-	hauteur = gets.to_i
+	hauteur = ask_pyramid_size
 	if hauteur%2 == 0
 		puts "Saisis un nombre impair stp !"
 	else
 		if (hauteur >= 1) && (hauteur <= 25)
 			puts "Voici la pyramide :"
 			1.upto(hauteur) do |i|
-				a = hauteur - i
-				a.times do
-					print " "
-				end
-				((i-1)*2 + 1).times do
-					print "#"
-				end
-				a.times do
-					print " "
-				end
-				puts ""
+				puts " "*(hauteur - i) + "#"*((i-1)*2 + 1)
 			end
 			(hauteur-1).downto(1) do |i|
-				a = hauteur - i
-				a.times do
-					print " "
-				end
-				((i-1)*2 + 1).times do
-					print "#"
-				end
-				a.times do
-					print " "
-				end
-				puts ""
+				puts " "*(hauteur - i) + "#"*((i-1)*2 + 1)
 			end
 		end
 	end
 end
 
+
+#half_pyramid
+#full_pyramid
 wtf_pyramid
