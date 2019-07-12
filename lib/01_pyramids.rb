cb7e98fec5f135d019f7d7d811618b4ee7a20d43
PaulMouchel <paulmouchel@live.fr>
2019-07-12 10:34:58 +0200

    improvements

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,48 +1,66 @@
 
-def ask_pyramid_size
-	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+def ask_floors
+	print "Combien d'étages veux-tu ?"
 	print "> "
-	h = gets.to_i
-	while (h<1 || h>25)
-		puts "Saisis un entre 1 et 25 stp !"
+	floors = gets.chomp.to_i
+	while ((floors < 1) || (floors > 25))
+		puts "Saisis un nombre entre 1 et 25 !"
 		print "> "
-		h = gets.to_i
+		floors = gets.chomp.to_i
 	end
-	return h
+	return floors
 end
 
-def check_parity(size)
-	while size.even?
-		puts "Saisis un nombre impair sinon c'est moche !"
-		size = ask_pyramid_size
+def check_parity(floors)
+	while floors.even?
+		puts "Saisis un nombre impair !!"
+		floors = ask_floors
 	end
-	return size
+	return floors
 end
 
-def half_pyramid
-	hauteur = ask_pyramid_size
+def half_pyramid(floors)
 	puts "Voici la pyramide :"
-	1.upto(hauteur) { |i| puts " "*(hauteur - i) + "#"*i }
+	1.upto(floors) { |i| puts " "*(floors - i) + "#"*i}
 end
 
-def full_pyramid
-	hauteur = ask_pyramid_size
+def full_pyramid (floors)
 	puts "Voici la pyramide :"
-	1.upto(hauteur) { |i| puts " "*(hauteur - i) + "#"*((i-1)*2 + 1) }
+	1.upto(floors) { |i| puts " "*(floors - i) + "#"*(i*2-1)}
 end
 
-def wtf_pyramid
-	hauteur = check_parity(ask_pyramid_size)
-	h = hauteur/2 + 1
-	puts "Voici la pyramide :"
-	1.upto(h) { |i| puts " "*(h - i) + "#"*((i-1)*2 + 1) }
-	(h-1).downto(1) { |i| puts " "*(h - i) + "#"*((i-1)*2 + 1) }
+def wtf_pyramid (floors)
+	floors = (check_parity(floors)) / 2 + 1
+	full_pyramid(floors)
+	(floors-1).downto(1) { |i| puts " "*(floors - i) + "#"*(i*2-1)}
+end
+
+def menu
+	puts "Quelle pyramide veux-tu ?"
+	puts "1 : Demi pyramide"
+	puts "2 : Pyramide"
+	puts "3 : WTF pyramide"
+	puts "0 : Quitter"
+	return gets.chomp.to_i
 end
 
 def perform
-	half_pyramid
-	full_pyramid
-	wtf_pyramid
+	puts "Salut, bienvenue dans ma super pyramide !"
+	while true
+		case menu
+		when 1
+			half_pyramid(ask_floors)
+		when 2
+		  	full_pyramid(ask_floors)
+		when 3
+		  	wtf_pyramid(ask_floors)
+		when 0
+			break
+		else
+		  	puts "Valeur non valide"
+		end
+		gets
+	end
 end
 
 perform
--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,51 +1,75 @@
-def throw_dice(auto_mode)
-	if !auto_mode
+def throw_dice(auto)
+	if !auto
 		puts "Appuyez sur entrée pour lancer le dé"
 		gets
 	end
 	return (1 + rand(6))
 end
 
-def try_to_go_up (dice_value, actual_step)
-	print "Vous avez fait un #{dice_value} "
-	if dice_value >= 5
-		actual_step += 1
-		puts "vous arrivez sur la marche #{actual_step}"
-	elsif dice_value <= 1 && actual_step >= 1
-		actual_step -= 1
-		puts "vous redesendez sur la marche #{actual_step}"
+def move(dice, step, auto)
+	print "Vous avez fait un #{dice} " if !auto
+	if dice >= 5
+		puts "Vous montez :)" if !auto
+		return 1
+	elsif dice <= 1 && step > 0
+		puts "vous redesendez :(" if !auto
+		return -1
 	else
-		puts "Vous restez sur la marche #{actual_step}"
+		puts "Rien ne se passe" if !auto
+		return 0
 	end
-	return actual_step
 end
 
-
-def play(auto_mode = false)
-	actual_step = 0
-	puts "Début du jeu"
-	puts "Vous être à la marche : #{actual_step}"
+def play(auto = false)
+	step = 0
 	turns = 0
-	while actual_step < 10
-		actual_step = try_to_go_up(throw_dice(auto_mode), actual_step)
+	puts "Début du jeu" if !auto
+	while step < 10
+		puts "Vous êtes sur la marche : #{step}" if !auto
+		step += move(throw_dice(auto), step, auto)
 		turns += 1
 	end
-	puts "Fin de partie, vous êtes en haut en #{turns} tours, Félicitations !!!!"
+	puts "Fin de partie, vous êtes en haut, Félicitations !!!!" if !auto
 	return turns
 end
 
 def average_finish_time
+	parties = 0
+	while parties <= 0
+		puts "Combien de parties voulez-vous lancer ?"
+		parties = gets.chomp.to_i
+	end
 	total = 0.0
-	100.times do
+	parties.times do
 		total += play(true)
 	end
-	average = total / 100
-	puts "Le nombre de tours moyen pour arriver en haut est : #{average} tours"
+	average = total / parties
+	puts "Le nombre de tours moyen pour arriver en haut sur les #{parties} parties est : #{average} tours"
+end
+
+def menu
+	puts "Bienvenue dans mon jeu, que veux-tu faire"
+	puts "1 : Partie normale"
+	puts "2 : Statistiques"
+	puts "0 : Quitter"
+	return gets.chomp.to_i
 end
 
 def perform
-	play
-	average_finish_time
+	while true
+		case menu
+		when 1
+			play
+		when 2
+		  	average_finish_time
+		when 0
+			break
+		else
+		  	puts "Valeur non valide"
+		end
+		gets
+	end
+	
 end
 
 perform
